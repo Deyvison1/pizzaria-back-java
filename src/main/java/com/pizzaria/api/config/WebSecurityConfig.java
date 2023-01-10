2abe245a40662e67c3be4c661c52f302a9761954
Deyvison <tutacf@gmail.com>
2023-01-09 22:27:13 -0300

    Teste

--- src/main/java/com/pizzaria/api/config/WebSecurityConfig.java
+++ src/main/java/com/pizzaria/api/config/WebSecurityConfig.java
@@ -2,10 +2,8 @@ package com.pizzaria.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,52 +15,50 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.pizzaria.api.config.jwt.impl.JwtAuthFilter;
 import com.pizzaria.api.config.jwt.impl.JwtServiceImpl;
+import com.pizzaria.api.models.enums.ROLES;
 import com.pizzaria.api.services.impl.UserDetailsServiceImpl;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
-    private UserDetailsServiceImpl usuarioService;
+	private UserDetailsServiceImpl usuarioService;
 	@Autowired
 	private JwtServiceImpl jwtService;
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-    
-    @Bean
-    public OncePerRequestFilter jwtFilter() {
-    	return new JwtAuthFilter(jwtService, usuarioService);
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .userDetailsService(usuarioService)
-            .passwordEncoder(passwordEncoder());
-    }
+	@Bean
+	public OncePerRequestFilter jwtFilter() {
+		return new JwtAuthFilter(jwtService, usuarioService);
+	}
 
-    @Override
-    protected void configure( HttpSecurity http ) throws Exception {
-    	http
-        .csrf().disable()
-        .authorizeRequests()
-            .antMatchers("/order/**")
-                .hasAnyRole("USER", "ADMIN")
-            .antMatchers("/category/**")
-                .hasAnyRole("USER", "ADMIN")
-            .antMatchers("/procut/**")
-                .hasRole("ADMIN")
-            .antMatchers(HttpMethod.POST, "/user/**")
-                .permitAll()
-            .anyRequest().authenticated()
-        .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-            .addFilterBefore( jwtFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		 http
+         .csrf().disable()
+         .authorizeRequests()
+             .antMatchers("/api/order/**")
+                 .hasAnyRole("USER", "ADMIN")
+             .antMatchers("/api/item/**")
+                 .hasAnyRole("USER", "ADMIN")
+             .antMatchers("/api/product/**")
+                 .hasRole("ADMIN")
+             .antMatchers(HttpMethod.POST, "/api/user/**")
+                 .permitAll()
+             .anyRequest().authenticated()
+         .and()
+             .sessionManagement()
+             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+         .and()
+             .addFilterBefore( jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
 }
-
